612536eb169c311e8d9a2c3cfab99dc414b8856e
Marcelo Taissoun <marcelotaissoun@gmail.com>
2026-01-27 15:31:30 -0300

    Create VeiculosController in C language.

--- Controllers/VeiculosController.c
+++ Controllers/VeiculosController.c
@@ -37,22 +37,22 @@ int proximoId = 1;
 
 // FUNÇÃO AUXILIAR DE BUSCA DE VEÍCULO POR SEU ID. COMPARA O ID INSERIDO COM OS EXISTENTES.
 int buscarPorId(int id){
-for(int i = 0; i < total; i++){
-   if(veiculos[i].id == id){
-   return i;
+   for(int i = 0; i < total; i++){
+      if(veiculos[i].id == id){
+      return i;
+      }
    }
-}
 return -1;
 }
 
 // INÍCIO DA CRUD
 // INDEX (UTILIZA LAÇO FOR PRA PERCORRER OS VEÍCULOS E IMPRIMIR UM POR UM):
 void listagemVeiculos(){
-if(total == 0){
-   printf("Nenhum veículo cadastrado.\n");
-   return;
-}
-printf("Lista de Veículos:");
+   if(total == 0){
+      printf("Nenhum veículo cadastrado.\n");
+      return;
+   }
+   printf("Lista de Veículos:");
    for(int i = 0; i < total; i++){
        printf("ID: %d | %s %s | Placa: %s", veiculos[i].id, veiculos[i].marca, veiculos[i].modelo, veiculos[i].placa);
    }
@@ -60,111 +60,111 @@ printf("Lista de Veículos:");
 
 // DETAILS (É USADO IDX E NÃO I POIS IDX REPRESENTA O ÍNDICE DO VEÍCULO ENCONTRADO; I É APENAS CONTROLE DO LAÇO DE REPETIÇÃO):
 void detalhesVeiculo(){
-int id;
-printf("Informe o ID do veículo: ");
-scanf("%d", &id);
+   int id;
+   printf("Informe o ID do veículo: ");
+   scanf("%d", &id);
 
-int idx = buscarPorId(id);
-if(idx == -1){
-   printf("Veículo não encontrado.");
-   return;
-}
+   int idx = buscarPorId(id);
+   if(idx == -1){
+      printf("Veículo não encontrado.");
+      return;
+   }
 
-Veiculo v = veiculos[idx];
+   Veiculo v = veiculos[idx];
 
-printf("Detalhes: ");
-printf("ID: %d", v.id);
-printf("Marca: %s", v.marca);
-printf("Modelo: %s", v.modelo);
-printf("Placa: %s", v.placa);
-printf("Ano: %d", v.ano);
-printf("Capacidade do tanque: %.2f", v.capacidadeTanque);
-printf("Combustível: %d", v.combustivel);
-printf("Categoria: %d", v.categoria);
+   printf("Detalhes: ");
+   printf("ID: %d", v.id);
+   printf("Marca: %s", v.marca);
+   printf("Modelo: %s", v.modelo);
+   printf("Placa: %s", v.placa);
+   printf("Ano: %d", v.ano);
+   printf("Capacidade do tanque: %.2f", v.capacidadeTanque);
+   printf("Combustível: %d", v.combustivel);
+   printf("Categoria: %d", v.categoria);
 }
 
 // CREATE (USA INT* EM COMBUSTÍVEL E CATEGORIA POIS FAZEM PARTE DE UMA ENUM, LOGO O INT* TRATA ELES COMO UM INTEIRO):
 void criarVeiculo(){
-if(total >= MAX){
-   printf("Limite de veículos atingido.");
-   return;
-}
+   if(total >= MAX){
+      printf("Limite de veículos atingido.");
+      return;
+   }
 
-Veiculo v;
-v.id = proximoId++; // SIMULA UM AUTO INCREMENT PRIMARY KEY DE SQL.
+   Veiculo v;
+   v.id = proximoId++; // SIMULA UM AUTO INCREMENT PRIMARY KEY DE SQL.
 
-printf("Placa: ");
-scanf("%s", v.placa);
+   printf("Placa: ");
+   scanf("%s", v.placa);
 
-printf("Marca: ");
-scanf("%s", v.marca);
+   printf("Marca: ");
+   scanf("%s", v.marca);
 
-printf("Modelo: ");
-scanf("%s", v.modelo);
+   printf("Modelo: ");
+   scanf("%s", v.modelo);
 
-printf("Ano: ");
-scanf("%d", &v.ano);
+   printf("Ano: ");
+   scanf("%d", &v.ano);
 
-printf("Capacidade do tanque: ");
-scanf("%f", &v.capacidadeTanque);
+   printf("Capacidade do tanque: ");
+   scanf("%f", &v.capacidadeTanque);
 
-printf("Combustível (0- Gasolina, 1- Álcool, 2- Diesel, 3- Elétrico): ");
-scanf("%d", (int*)&v.combustivel);
+   printf("Combustível (0- Gasolina, 1- Álcool, 2- Diesel, 3- Elétrico): ");
+   scanf("%d", (int*)&v.combustivel);
 
-printf("Categoria (0-Passeio, 1-SUV, 2-Caminhão, 3-Moto): ");
-scanf("%d", (int*)&v.categoria);
+   printf("Categoria (0-Passeio, 1-SUV, 2-Caminhão, 3-Moto): ");
+   scanf("%d", (int*)&v.categoria);
 
-veiculos[total] = v;
-total++; // INCREMENTA NO TOTAL DE VEÍCULOS CADASTRADOS, RESPEITANDO O MÁX DE 100 VEÍCULOS.
+   veiculos[total] = v;
+   total++; // INCREMENTA NO TOTAL DE VEÍCULOS CADASTRADOS, RESPEITANDO O MÁX DE 100 VEÍCULOS.
 
-printf("Veículo cadastrado com sucesso!");
+   printf("Veículo cadastrado com sucesso!");
 }
 
 // EDIT:
 void editarVeiculo(){
-int id;
-printf("Informe o ID do veículo: ");
-scanf("%d", &id);
+   int id;
+   printf("Informe o ID do veículo: ");
+   scanf("%d", &id);
 
-int idx = buscarPorId(id);
-if(idx == -1){
-   printf("Veículo não encontrado.");
-   return;
-}
+   int idx = buscarPorId(id);
+   if(idx == -1){
+      printf("Veículo não encontrado.");
+      return;
+   }
 
-printf("Nova placa: ");
-scanf("%s", veiculos[idx].placa);
+   printf("Nova placa: ");
+   scanf("%s", veiculos[idx].placa);
 
-printf("Nova marca: ");
-scanf("%s", veiculos[idx].marca);
+   printf("Nova marca: ");
+   scanf("%s", veiculos[idx].marca);
 
-printf("Novo modelo: ");
-scanf("%s", veiculos[idx].modelo);
+   printf("Novo modelo: ");
+   scanf("%s", veiculos[idx].modelo);
 
-printf("Novo ano: ");
-scanf("%d", &veiculos[idx].ano);
+   printf("Novo ano: ");
+   scanf("%d", &veiculos[idx].ano);
 
-printf("Atualizado com sucesso!");
+   printf("Atualizado com sucesso!");
 }
 
 // DELETE:
 void excluirVeiculo(){
-int id;
-printf("Informe o ID do veículo: ");
-scanf("%d", &id);
+   int id;
+   printf("Informe o ID do veículo: ");
+   scanf("%d", &id);
 
-int idx = buscarPorId(id);
-if(idx == -1){
-   printf("Veiculo nao encontrado.\n");
-   return;
-}
+   int idx = buscarPorId(id);
+   if(idx == -1){
+      printf("Veiculo nao encontrado.\n");
+      return;
+   }
 
-for(int i = idx; i < total - 1; i++){ // REORGANIZA O ARRAY DE VEÍCULOS APÓS A EXCLUSÃO DE UM VEÍCULO.
-veiculos[i] = veiculos[i + 1];
-}
+   for(int i = idx; i < total - 1; i++){ // REORGANIZA O ARRAY DE VEÍCULOS APÓS A EXCLUSÃO DE UM VEÍCULO.
+       veiculos[i] = veiculos[i + 1];
+   }
 
-total--;
-printf("Veículo excluído com sucesso!");
+   total--;
+   printf("Veículo excluído com sucesso!");
 }
 
 // FUNÇÃO MAIN QUE USA TODAS AS FUNÇÕES CRIADAS.
