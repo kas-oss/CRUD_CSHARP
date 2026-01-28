a5ffcef7087c145870d23f91823da9860ef401b7
Filipe <filipe.ribeirosilva22@gmail.com>
2026-01-28 20:27:42 -0300

    Feat: Exibir dados em vez dos IDs das entidades relacionadas

--- Controllers/VinculacaoController.cs
+++ Controllers/VinculacaoController.cs
@@ -5,21 +5,41 @@ namespace CRUD_CSHARP.Controllers;
 
 public class VinculacaoController : Controller
 {
-
     public VinculacaoRepository _vinculacaoRepository;
+
+    public MotoristaRepository _motoristaRepository;
+    public VeiculoRepository _veiculoRepository;
 
     public static int _nextId = 2;
 
-    public VinculacaoController(VinculacaoRepository vinculacaoRepository)
+    public VinculacaoController(
+        VinculacaoRepository vinculacaoRepository,
+        MotoristaRepository motoristaRepository,
+        VeiculoRepository veiculoRepository
+    )
     {
         _vinculacaoRepository = vinculacaoRepository;
+        _motoristaRepository = motoristaRepository;
+        _veiculoRepository = veiculoRepository;
     }
 
     public IActionResult Index()
     {
-        var lista = _vinculacaoRepository.Vinculacoes.OrderBy(v => v.Id).ToList();
+        var vincs = _vinculacaoRepository.Vinculacoes.OrderBy(v => v.Id).ToList();
 
-        return View(lista);
+        var fullVincs = vincs.Select(vinc =>
+        {
+            var motorista = _motoristaRepository.Motoristas.FirstOrDefault(m => m.Id == vinc.MotoristaId);
+            var veiculo = _veiculoRepository.Veiculos.FirstOrDefault(v => v.Id == vinc.VeiculoId);
+
+            return new VinculacaoFullModel
+            {
+                Vinculacao = vinc,
+                Motorista = motorista,
+                Veiculo = veiculo,
+            };
+        });
+
+        return View(fullVincs);
     }
-    
 }
